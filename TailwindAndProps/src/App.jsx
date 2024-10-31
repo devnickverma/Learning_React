793666e3c4847f6a627ad53b642584e7ae58d447
Nikhil Verma <devnickverma@gmail.com>
2024-10-31 11:44:33 +0530

    Tailwind Css

---
+++ TailwindAndProps/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/card'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className = 'bg-green-500 text-black p-5 rounded-lg'>Tailwind css</h1>
+      <br />
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+    </>
+  )
+}
+
+export default App
