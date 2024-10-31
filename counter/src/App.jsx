78d120c1ee6574db29c818760d42893408ba63d9
Nikhil Verma <devnickverma@gmail.com>
2024-10-31 18:57:10 +0530

    Set max and min value in counter app

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -6,16 +6,27 @@ import './App.css'; // Importing CSS file for styling the App component
 function App() {
   // Declaring a state variable 'counter' initialized to 15, and 'setCounter' function to update it
   const [counter, setCounter] = useState(15);
+  const [message, setMessage] = useState(''); // State for message display
 
   // Function to increase the counter value by 1
   const addValue = () => {
-    setCounter(counter + 1); // Updating state using setCounter function
-  }
+    if (counter < 20) {
+      setCounter(counter + 1); // Updating state using setCounter function
+      setMessage(''); // Clear message
+    } else {
+      setMessage('Maximum limit reached'); // Set message if max limit is reached
+    }
+  };
 
   // Function to decrease the counter value by 1
-  const removevalue = () => {
-    setCounter(counter - 1); // Updating state using setCounter function
-  }
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1); // Updating state using setCounter function
+      setMessage(''); // Clear message
+    } else {
+      setMessage('Minimum limit reached'); // Set message if min limit is reached
+    }
+  };
 
   // The component's rendered output
   return (
@@ -24,22 +35,16 @@ function App() {
 
       <h2>Counter value : {counter}</h2> {/* Displaying the current value of the counter */}
       
-      <button
-        onClick={addValue} // Setting up an onClick event to call addValue function
-      >
-        Add Value
-      </button>
+      <button onClick={addValue}>Add Value</button> {/* Add Value button */}
 
       <br /> {/* Line break for spacing */}
       <br /> {/* Line break for spacing */}
 
-      <button 
-        onClick={removevalue} // Setting up an onClick event to call removevalue function
-      >
-        Remove Value
-      </button>
+      <button onClick={removeValue}>Remove Value</button> {/* Remove Value button */}
+
+      {message && <p>{message}</p>} {/* Display message if it exists */}
     </>
-  )
+  );
 }
 
 export default App; // Exporting the App component as default for use in other files
